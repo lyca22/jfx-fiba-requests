8ac6e869898a2b4c50ac0a94bf4b6b013d8462f4
lyca22 <ariep2206@gmail.com>
2021-11-01 18:00:19 -0500

    Implemented equalSearchList, completed searchList and made minor changes to delete in BinaryTree. Fixed balance and implemented delete in AVLTree. Made minor fixes for other classes

--- src/datastr/IAVLTree.java
+++ src/datastr/IAVLTree.java
@@ -1,9 +1,11 @@
 package datastr;
 
-public interface IAVLTree<E extends Comparable<E>, P extends Comparable<P>> {
+public interface IAVLTree<E, P extends Comparable<P>> {
 	
 	public Node<E, P> leftRotate(Node<E, P> node);
 	public Node<E, P> rightRotate(Node<E, P> node);
 	public void balance(Node<E, P> node);
+	public int getBalance(Node<E, P> node);
+	public int getHeight(Node<E, P> node);
 	
 }
