6594fe142804a5d79158d102409a08b1fe5d8798
Julian Bolanos <julian.camilob24@gmail.com>
2021-11-01 15:25:58 -0500

    Changes in setup of jdk project

--- src/ui/FIBARequestsController.java
+++ src/ui/FIBARequestsController.java
@@ -1,5 +1,12 @@
 package ui;
 
+import javafx.fxml.FXML;
+import javafx.scene.layout.AnchorPane;
+
 public class FIBARequestsController {
 
+	//mainPane fields
+	@FXML
+    private AnchorPane mainPane;
+	
 }
