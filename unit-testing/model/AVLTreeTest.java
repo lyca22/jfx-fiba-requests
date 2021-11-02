eb83c3baf894d31dc9ae518f09e2c32d9c2f3300
JuanFeBlancoT <juanfeblancot@gmail.com>
2021-11-02 09:28:22 -0500

    model tests runned successfully

--- unit-testing/model/AVLTreeTest.java
+++ unit-testing/model/AVLTreeTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import datastr.AVLTree;
-import datastr.Node;
 
 class AVLTreeTest {
 
--- unit-testing/model/FIBARequestsTest.java
+++ unit-testing/model/FIBARequestsTest.java
@@ -1,42 +1,50 @@
 package model;
+import static org.junit.Assert.assertTrue;
 
-
-import java.util.ArrayList;
-
-import org.junit.Test;
 import org.junit.jupiter.api.BeforeEach;
-
-import datastr.AVLTree;
-import datastr.BinaryTree;
-
+import org.junit.jupiter.api.Test;
 
 class FIBARequestsTest {
 
-	private ArrayList<Player> playerList;
-	private AVLTree<Player, Double> averageScoreTree;
-	private AVLTree<Player, Double> averageAssistsTree;
-	private AVLTree<Player, Double> averageBlocksTree;
-	private AVLTree<Player, Double> averageStealsTree;
-	private AVLTree<Player, Double> averageBouncesTree;
+	FIBARequests fib; 
 	
 	@BeforeEach
 	void FIBASC1() throws Exception {
-		playerList = new ArrayList<>();
-		averageScoreTree = new AVLTree<>();
-		averageBouncesTree = new AVLTree<>();
-		averageAssistsTree = new AVLTree<>();
+		fib = new FIBARequests();
 	}
 	
 	@BeforeEach
 	void FIBASC2() throws Exception {
-		playerList = new ArrayList<>();
-		averageScoreTree = new AVLTree<>();
-		averageBouncesTree = new AVLTree<>();
-		averageAssistsTree = new AVLTree<>();
+		fib = new FIBARequests();
+		Player p = new Player("Juancarie", 23, "NOCHU", 5.5, 4.3, 2.8, 7.5, 6);
+		fib.addPlayer(p);
+		
 	}
 
 	@Test
-	void testAddPlayer() {
+	void testAddPlayer() throws Exception {
+		System.out.println("ddgfg");
+		FIBASC1();
+		Player p = new Player("Jose", 34, "MECHU", 6,3,5,8,9);
+		fib.addPlayer(p);
 		
+		assertTrue(fib.getAverageScoreTree().getRoot().getSearchParameter()==6);
+		assertTrue(fib.getAverageAssistsTree().getRoot().getSearchParameter()==3);
+		assertTrue(fib.getAverageBlocksTree().getRoot().getSearchParameter()==5);
+		assertTrue(fib.getAverageStealsTree().getRoot().getSearchParameter()==8);
+		assertTrue(fib.getAverageBouncesTree().getRoot().getSearchParameter()==9);
+		
+		FIBASC2();
+		Player p2 = new Player("Jose", 34, "MECHU", 6,3,5,8,9);
+		fib.addPlayer(p2);
+		
+		assertTrue(fib.getPlayerList().size()==2);
+		assertTrue(fib.getAverageScoreTree().getRoot().getRight() != null);
+		assertTrue(fib.getAverageScoreTree().getRoot().getRight().getSearchParameter() == 6);
+		
+		assertTrue(fib.getAverageAssistsTree().getRoot().getLeft() != null);
+		assertTrue(fib.getAverageBlocksTree().getRoot().getRight() != null);
+		assertTrue(fib.getAverageStealsTree().getRoot().getRight() != null);
+		assertTrue(fib.getAverageBouncesTree().getRoot().getRight() != null);
 	}
 }
