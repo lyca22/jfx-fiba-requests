493160048124d5232e93771db4cec6de10023e81
JuanFeBlancoT <juanfeblancot@gmail.com>
2021-11-02 06:27:25 -0500

    AVLTreeTest cases added

---
+++ unit-testing/model/AVLTreeTest.java
@@ -0,0 +1,116 @@
+package model;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import datastr.AVLTree;
+
+class AVLTreeTest {
+
+	AVLTree<Integer, Integer> tree;
+	
+	@BeforeEach
+	void AVLTSC1() throws Exception {
+		tree = new AVLTree<>();
+	}
+	
+	@BeforeEach
+	void AVLTSC2() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(9, 9);
+		tree.add(5, 5);
+		tree.add(11, 11);
+	}
+	
+	@BeforeEach
+	void AVLTSC3() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(74, 74);
+		tree.add(28, 28);
+		tree.add(50, 50);
+		tree.add(19, 19);
+		tree.add(60, 60);
+	}
+	
+	@BeforeEach
+	void AVLTSC4() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(74, 74);
+		
+		tree.add(16, 16);
+		tree.add(28, 28);
+		tree.add(50, 50);
+		tree.add(90, 90);
+	}
+	
+	@BeforeEach
+	void AVLTSC5() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(74, 74);
+		
+		tree.add(16, 16);
+		tree.add(60, 60);
+		tree.add(90, 90);
+		tree.add(95, 95);
+	}
+	
+	@BeforeEach
+	void AVLTSC6() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(74, 74);
+		
+		tree.add(16, 16);
+		tree.add(60, 60);
+		tree.add(90, 90);
+		tree.add(55, 55);
+		tree.add(65, 65);
+	}
+	
+	@BeforeEach
+	void AVLTSC7() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(90, 90);
+		
+		tree.add(16, 16);
+		tree.add(60, 60);
+		tree.add(55, 55);
+		tree.add(65, 65);
+		tree.add(64, 64);
+	}
+
+	@BeforeEach
+	void AVLTSC8() throws Exception {
+		tree = new AVLTree<>();
+		tree.add(45, 45);
+		tree.add(18, 18);
+		tree.add(74, 74);
+		
+		tree.add(16, 16);
+		tree.add(65, 65);
+		tree.add(90, 90);
+		tree.add(60, 60);
+		tree.add(55, 55);
+		tree.add(64, 64);
+	}
+	
+	@Test
+	void testAdd() throws Exception {
+		AVLTSC1();
+		System.out.println(tree.getRoot()==null);
+		tree.add(7, 7);
+		assertTrue(true);
+		
+	}
+}
